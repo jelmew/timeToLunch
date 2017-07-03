e7f8bf7e9780533fdec7677a7fa10afc7c7c66c7
Jelmer van Amen <Jelmer.van.Amen@crv4all.com>
2017-07-03 13:44:09 +0200

    Initial checkin

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    int seconds = 60 - ltm->tm_sec;
+    int minutes = 60 - 1 - ltm->tm_min;
+    int hours = 12 - 1 - ltm->tm_hour;
+    if (hours < 0) {
+        cout << "We are already past lunchtime lazy fucker" << endl;
+        return 0;
+    }
+    cout << hours << " :hours, " << minutes << " :minutes" << seconds << " : seconds to lunch" << endl;
+    return 0;
+}
+
